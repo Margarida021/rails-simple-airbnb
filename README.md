776916b1467eb53b5f5db5ad5761acd66970df2e
Margarida Almeida <margarida.almeida.92@gmail.com>
2023-08-16 17:30:30 +0100

    Simple AirBnb Not-Finished

--- README.md
+++ README.md
@@ -22,3 +22,10 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+
+<!-- As a user, I can see all the available flats on our website -->
+As a user, I can post a flat to the website, specifying its name and address
+As a user, I can see detailed information of a given flat
+As a user, I can edit the details of a flat if I made a mistake
+As a user, I can delete a flat from the website, in case I don’t want to rent it anymore
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Destroying the previous data..."
+Flat.destroy_all
+puts "All done!"
+
+8.times do
+  Flat.create!(
+    name: "Light & Spacious Garden Flat London",
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    price_per_night: Faker::Number.number(digits: 3),
+    number_of_guests: Faker::Number.within(range: 1..6)
+  )
+end
